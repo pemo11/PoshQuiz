4d51888cb5204fc2ee5b3ccef710c117b0efe4dc
Peter <pm@activetraining.de>
2018-02-26 10:38:07 +0100

    Minor bug fixes and a menu for repeating a quiz - not working version

--- PoshQuizMsgTable.psd1
+++ PoshQuizMsgTable.psd1
@@ -1,7 +1,10 @@
 
 ConvertFrom-StringData -StringData @"
     QuizLoadedMsg = {0} quizzes loaded
+    QuizNotFoundMsg = There is no Quizz with that Id - please try again
     ShowMenuShowQuizzesMsg = Show available quizzes
     ShowMenuStartQuizMsg = Start a quizz
     ShowMenuEndMsg = The End
+    QuizEndTryAgainMsg = Run this Quizz again?
+    QuizEndTryWrongAnswersMsg = Repeat only the wrong answers?
 "@
--- PoshQuizV1.ps1
+++ PoshQuizV1.ps1
@@ -7,9 +7,7 @@ Import-LocalizedData -FileName PoshQuizMsgTable.psd1 -BindingVariable PoshQuizMs
 
 . .\PoshQuizHelpersV1.ps1
 
-$QuizPath = ".\Quizzes"
-
-$QuizTable = @{}
+$QuizTable =[Ordered]@{}
 $CurrentQuiz = $null
 
 <#
@@ -19,48 +17,79 @@ Loads all quiz defs as Json files in the quizzes subdirectory
 function LoadQuiz
 {
     [CmdletBinding()]
-    param()
+    param([String]$QuizPath = ".\Quizzes")
 
     Get-ChildItem -path $QuizPath\*.json | ForEach-Object {
         $Quiz = (Get-Content -Path $_.FullName -Encoding Default ) | ConvertFrom-Json
-        $Script:QuizTable.Add($Quiz.QuizId, $Quiz)
+        # $Script:QuizTable.Add($Quiz.QuizId, $Quiz)
+        $Script:QuizTable += @{$Quiz.QuizId = $Quiz}
     }
     Write-Verbose ($PoshQuizMsg.QuizLoadedMsg -f $QuizTable.Values.Count)
 }
 
+$MainMenu = @()
+$MainMenu += "1 - $($PoshQuizMsg.ShowMenuShowQuizzesMsg)"
+$MainMenu += "2 - $($PoshQuizMsg.ShowMenuStartQuizMsg)"
+$MainMenu += "Q - $($PoshQuizMsg.ShowMenuEndMsg)"
 
+$QuizEndMenu = @()
+$QuizEndMenu += "1 - $($PoshQuizMsg.QuizEndTryAgainMsg)"
+$QuizEndMenu += "2 - $($PoshQuizMsg.QuizEndTryWrongAnswersMsg)"
+$QuizEndMenu += "Q - $($PoshQuizMsg.ShowMenuEndMsg)"
+
+<#
+.Synopsis
+Displays a multiple choice menu
+#>
 function ShowMenu
 {
     [CmdletBinding()]
-    param()
+    param([String[]]$MenuItems)
 
-    Write-Host "1) $($PoshQuizMsg.ShowMenuShowQuizzesMsg)"
-    Write-Host "2) $($PoshQuizMsg.ShowMenuStartQuizMsg)"
-    Write-Host "Q - $($PoshQuizMsg.ShowMenuEndMsg)"
-    $Choice = Read-Host -Prompt "1,2 or Q?"
+    Foreach($MenuItem in $MenuItems)
+    {
+        Write-Host $MenuItem
+    }
+    $ChoicePrompt = ((0..($MenuItems.Count -2)).ForEach{[Char]($_+49)} -join ",") + " or Q?"
+    $Choice = Read-Host -Prompt $ChoicePrompt
     $Choice.ToUpper()
 }
 
+<#
+.Synopsis
+Displays all available quizzes
+#>
 function ShowQuizTable
 {
     [CmdletBinding()]
     param()
     $i = 0
     $QuizTable.Values.ForEach{
-        $i++
-        $LineOutput = "{0}) {1} - {2}" -f $i, $_.Title, $_.QuizId   
-        Write-Host $LineOutput
+        $Outline = "{0} - {1}/{2} {3}" -f ++$i, $_.Title, $_.QuizID, $_.Author
+        Write-Host $Outline
     }
+    Write-Host
 }
 
+<#
+.Synopsis
+Start a quizz
+#>
 function StartQuiz
 {
     [CmdletBinding()]
     param([Object]$Quiz)
     $QuizInitPrompt = "Choose a Quiz by ID (" + ($QuizTable.Keys -join ",") + ")"
     [Int32]$QuizId = Read-Host $QuizInitPrompt
-    $Script:CurrentQuiz = $QuizTable[$QuizId]
-    Invoke-QuizRun -Quiz $CurrentQuiz
+    if ($QuizTable.Keys -contains $QuizId)
+    {
+        $Script:CurrentQuiz = $QuizTable[$QuizId]
+        Invoke-QuizRun -Quiz $CurrentQuiz
+        ShowMenu $QuizEndMenu
+    }
+    else {
+        Write-Host -Fore Red $PoshQuizMsg.QuizNotFoundMsg
+    }
 }
 
 LoadQuiz -Verbose
@@ -69,7 +98,7 @@ LoadQuiz -Verbose
 do
 {
     $ExitMode = $false
-    $Choice = ShowMenu
+    $Choice = ShowMenu $MainMenu
     switch ($Choice)
     {
         "1" {
